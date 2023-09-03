001ff867584c0e69113ce758fabd17e8b752e3b2
=drdexter33 <=drdexter33@protonmail.com>
2023-09-03 14:26:03 -0400

    inital checkin

---
+++ src/server/routes.js
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------------
++ References the Express router
++ Gets the heroes
++ Sends back the JSON for a defined hero
+----------------------------------------------------------------*/
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/heroes', (req, res) => {
+ res.send(200, [
+    {"id": 10, "name": "Starlord", "saying": "oh yeah"}
+ ])
+});
+
+module.exports=router;
