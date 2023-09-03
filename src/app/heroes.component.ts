f5db604add8a65374198403837884c65c6f51c9e
=drdexter33 <=drdexter33@protonmail.com>
2023-09-03 19:15:07 -0400

    checkin

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -2,14 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
-  template: `
-    <p>
-      heroes works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
+  addingHero = false;
+  heroes: any= [];
+  selectedHero: Hero;
 
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  cancel() {
+    this.addingHero = false;
+    this,this.selectedHero = null;
+  }
+
+  deleteHero(hero: Hero){
+    this.heroService.deleteHero(hero).subscribe(res => {
+      this.heroes = this.heroes.filter(h => h != hero);
+      if(this.selectedHero === hero){
+        this.selectedHero = null;
+      }
+    });
+  }
+
+  getHeroes(){
+    return this.heroService.getHeroes.subscribe(heroes => {
+
+    });
+
+  };
 }
